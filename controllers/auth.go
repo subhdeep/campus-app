3cfc325c68c82c7feb64654905f13387d1f3ffec
Subhdeep Saha <subhdeepsaha@gmail.com>
2018-09-11 23:22:29 +0530

    WIP Implements SMTP login

--- controllers/auth.go
+++ controllers/auth.go
@@ -1,6 +1,9 @@
 package controllers
 
 import (
+	"fmt"
+	"net/smtp"
+
 	"github.com/kataras/iris"
 )
 
@@ -16,6 +19,18 @@ type LoginResponse struct {
 	Password string `json:"password"`
 }
 
+// unencryptedAuth struct
+type unencryptedAuth struct {
+	smtp.Auth
+}
+
+// Implements unencryptedAuth
+func (a unencryptedAuth) Start(server *smtp.ServerInfo) (string, []byte, error) {
+	s := *server
+	s.TLS = true
+	return a.Auth.Start(&s)
+}
+
 // Login of a user
 func Login(ctx iris.Context) {
 
@@ -25,7 +40,38 @@ func Login(ctx iris.Context) {
 		ctx.StatusCode(iris.StatusBadRequest)
 		ctx.Text(errReq.Error())
 	} else {
-		ctx.StatusCode(iris.StatusOK)
-		ctx.JSON(LoginResponse{Username: user.Username, Password: user.Password})
+		if checkLoginCred(&user) {
+			fmt.Println("Connection is successfull")
+			ctx.StatusCode(iris.StatusOK)
+		} else {
+			ctx.StatusCode(iris.StatusForbidden)
+		}
 	}
+}
+
+func checkLoginCred(cred *LoginCred) bool {
+	hostname := "smtp.cc.iitk.ac.in"
+	conn, err := smtp.Dial(hostname + ":25")
+	if err != nil {
+		fmt.Println(err.Error())
+		fmt.Println("Connection Failed")
+		return false
+	}
+	fmt.Println(cred)
+	defer conn.Close()
+	auth := unencryptedAuth{
+		smtp.PlainAuth(
+			"",
+			cred.Username,
+			cred.Password,
+			hostname,
+		),
+	}
+	err = conn.Auth(auth)
+	if err != nil {
+		fmt.Println(err.Error())
+		fmt.Println("Authentication Failed")
+		return false
+	}
+	return true
 }
